86686eb6b9b7000ef845b2d47b4ea23f0d52b6f1
somekindofgodofwar <a3s47k@gmail.com>
2019-05-24 13:50:09 +0700

    index
    index

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,73 @@
 $(document).ready(function () {
 	// Viết Script ở đây!
-	alert("fuck u");
+	var speed = 500;
+	$('#sidebar-index-wrappper > li.have-children > a').click(function (e) {
+		e.preventDefault();
+		if (!$(this).parent().hasClass('active')) {
+			$('#sidebar-index-wrappper li ul').slideUp(speed);
+			$(this).next().slideToggle(speed);
+			$('#sidebar-index-wrappper li').removeClass('active');
+			$(this).parent().addClass('active');
+		} else {
+			$(this).next().slideToggle(speed);
+			$('#sidebar-index-wrappper li').removeClass('active');
+		}
+	});
+
+	swiperInit();
 });
+
+
+
+function swiperInit() {
+	var mySwiper = new Swiper('#index-swiper', {
+		direction: 'horizontal',
+		slidesPerView: 1,
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+	var mySwiper = new Swiper('#index-news-swiper', {
+		direction: 'horizontal',
+		slidesPerView: 1,
+		loop: true,
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true
+		},
+		navigation: {
+			nextEl: '.index-news-swiper-prev',
+			prevEl: '.index-news-swiper-next',
+		},
+	});
+	var mySwiperPartner = new Swiper('#index-news-partner-vertical', {
+		direction: 'vertical',
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		loopedSlides :'auto',
+		autoplay: {
+			delay: 500,
+		},
+		speed: 700,
+
+	});
+	var indexSwiperCatalouge = new Swiper('#index-guide-swiper-catalogue', {
+		direction: 'horizontal',
+		slidesPerView: 1,
+		navigation: {
+			nextEl: '.swiper-catelogue-next',
+			prevEl: '.swiper-catelogue-prev',
+		  },
+		autoplay: {
+			delay: 500,
+		},
+		speed: 700,
+
+	});
+	// mySwiperPartner.autoplay.start();
+
+
+
+}
